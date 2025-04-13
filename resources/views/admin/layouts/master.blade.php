e525516797596f40a35f1a4a3d81b692bf2fb08c
Otnielkalit <otnielkalit25@gmail.com>
2025-04-13 22:50:35 +0700

    Implement About resource with CRUD functionality and update routes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -3,8 +3,9 @@
 
 <head>
     <meta charset="utf-8">
+    <base href="/public">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Skydash Admin</title>
+    <title>{{ $title }}</title>
     <link rel="stylesheet" href="assets-admin/vendors/feather/feather.css">
     <link rel="stylesheet" href="assets-admin/vendors/ti-icons/css/themify-icons.css">
     <link rel="stylesheet" href="assets-admin/vendors/css/vendor.bundle.base.css">
