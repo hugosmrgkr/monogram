272ec1ac180ff427d61a210505fbc3b24d6f76e0
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-05-14 21:59:42 +0700

    ganti seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,8 +14,8 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         Admin::create([
-            'email'    => 'admin@gmail.com',
-            'password' => Hash::make('12345678'),
+            'email'    => 'adminmono@gmail.com',
+            'password' => Hash::make('<mono_;'),
         ]);
     }
 }
