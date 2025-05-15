14d129b6c69179ad6a1a466aa4c4a9b507fd7fef
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-05-15 09:30:28 +0700

    l

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monogram Toba - @yield('title', 'Photography Studio')</title>
+    <title>Monogram Balige - @yield('title', 'Photography Studio')</title>
     <link rel="icon" href="{{ asset('assets/images/logo-user.jpg') }}" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
