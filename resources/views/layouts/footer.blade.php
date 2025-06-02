f1328a3fbdd3e203582db672848a38c77b59c625
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-06-02 09:08:33 +0700

    simple footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,9 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <h5 class="footer-title">Monogram Toba</h5>
-                <p>Studio fotografi profesional di Balige sejak 17 Agustus 2022. Menyediakan layanan dokumentasi momen
-                    spesial seperti wisuda, prewedding, pernikahan, keluarga, dan lainnya.</p>
-                <!-- Lokasi Google Maps -->
+                <p>Studio fotografi profesional di Balige sejak 17 Agustus 2022. Melayani dokumentasi wisuda, prewedding, pernikahan, keluarga, dan lainnya.</p>
                 <div class="google-maps">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.579601644355!2d98.98105411643752!3d2.3945973539259866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3131eaa0c1a132d1%3A0x87229c590207c674!2s83J8%2BPQ6%2C%20Lumban%20Dolok%20Haume%20Bange%2C%20Balige%2C%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sen!2sid!4v1678444263577!5m2!1sen!2sid"
@@ -13,14 +11,14 @@
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <h5 class="footer-title">Kontak</h5>
-                <p>📍 83J8+PQ6, Lumban Dolok Haume Bange, Kec. Balige, Toba, Sumatera Utara</p>
-                <p>📞 +62 82268691532</p>
+                <p>📍 Lumban Dolok Haume Bange, Balige, Toba</p>
+                <p>📞 +62 822 6869 1532</p>
                 <p>✉️ jeremysmrngkr@gmail.com</p>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <h5 class="footer-title">Layanan</h5>
                 <ul>
                     <li>Foto Studio Murah dan Profesional</li>
@@ -28,15 +26,6 @@
                     <li>Foto Prewedding yang Mengesankan</li>
                     <li>Dokumentasi Acara Keluarga</li>
                     <li>Fotografi Pernikahan Tak Terlupakan</li>
-                </ul>
-            </div>
-
-            <div class="col-md-3">
-                <h5 class="footer-title">Informasi</h5>
-                <ul>
-                    <li><a>Kebijakan Privasi</a></li>
-                    <li><a>Syarat & Ketentuan</a></li>
-
                 </ul>
             </div>
         </div>
