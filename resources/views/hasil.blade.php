ddaf8fdaa82303aa14353bc516ad2d4281dae17f
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-06-02 14:56:34 +0700

    8

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -4,7 +4,7 @@
 
 @section('styles')
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/gallery.css?v=999') }}">
+    <link rel="stylesheet" href="{{ asset('css/gallery.css?v=998') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- AOS CSS -->
